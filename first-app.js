72f739299a28ce0a4e3a829f90c2d451176f83ef
tanmaysihare <tanmaysihare@gmail.com>
2024-02-24 13:43:03 +0530

    create server in node.js

--- first-app.js
+++ first-app.js
@@ -1,6 +1,14 @@
-const productOfTwoNumbers = () => {
-let a = 2;
-let b = 4;
-let c = a+b ;
-return c;
+console.log('a');
+console.log('b');
+const delay =()=> {
+    const promise = new Promise((resolve)=>{
+        setTimeout(()=> {resolve(console.log('c'))},2000);
+    });
+    return promise;
 }
+async function printD(){
+    await delay();
+    console.log('d');
+    console.log('e');
+}
+printD();
