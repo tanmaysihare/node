2efca1e296bd3aae8c0e5c6525075869624c44a7
tanmaysihare <tanmaysihare@gmail.com>
2024-02-26 12:01:50 +0530

    cleaning up the code

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    
- 
-      if (url === '/') {
-        fs.readFile('message.txt','utf8',data=>{
-        res.write('<html>');
-        res.write('<head><title>enter message PAGE</title></head>');
-        res.write('<body>');
-        res.write(`<h1>${data}</h1>`);
-        console.log(data);
-        res.write('<form action="/massage" method="POST"><input type="text" name="message"/><button type="submit">send</button></form>');    
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-    })
-    } else if (url === '/massage' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=> {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location','/');
-                return res.end();
-            });
-        });
-       } 
-    // res.setHeader('Content-Type', 'text/html');
-    // res.write('<html>');
-    // res.write('<head><title>HOME PAGE</title></head>');
-    // res.write('<body>');
-    // res.write('<h1>hello  i am Tanmay Sihare</h1>');
-    // res.write('</body>');
-    // res.write('</html>');
-    // res.end();
-});
+const routes = require('./routes');
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const requestHandler = (req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        fs.readFile('message.txt',{encoding: "utf-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+        res.write('<html>');
+        res.write('<head><title>enter message PAGE</title></head>');
+        res.write('<body>');
+        res.write(`<h1>${data}</h1>`);
+        console.log(data);
+        res.write('<form action="/massage" method="POST"><input type="text" name="message"/><button type="submit">send</button></form>');    
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    })
+    } else if (url === '/massage' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+      return req.on('end',()=> {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt',message, err => {
+                if(err){
+                    console.log(err);
+                }
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        });
+       } else{
+        res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>HOME PAGE</title></head>');
+    res.write('<body>');
+    res.write('<h1>hello  i am Tanmay Sihare</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+       }
+};
+exports.handler = requestHandler;
