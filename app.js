fa752501af4d0f88ae88f916751a4c389368a27e
tanmaysihare <tanmaysihare@gmail.com>
2024-02-26 13:29:57 +0530

    getting familiar with express.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const http = require('http');
-
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
-
-server.listen(4000);
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+    console.log('hello');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('hello2');
+    res.send('<h1>hello from me</h1>')
+});
+app.listen(3300);
